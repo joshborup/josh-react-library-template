a93030ed144a5c6fa7356586ff4a619f03f94afc
joshborup <joshborup@gmail.com>
2023-11-23 16:24:02 -0700

    adding repo and license

--- lib/components/Button/index.jsx
+++ lib/components/Button/index.jsx
@@ -1,3 +1,8 @@
 
+import PropTypes from 'prop-types';
 import styles from './styles.module.css'
-export const Button = () => <div className={styles.button}>Hello</div>
+export const Button = ({name}) => <div className={styles.button}>Hello {name}</div>
+
+Button.propTypes = {
+  name: PropTypes.string
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Button } from '../'
 
 import './App.css'
@@ -6,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <Button />
+      <Button name={'josh'}/>
     </div>
   )
 }
