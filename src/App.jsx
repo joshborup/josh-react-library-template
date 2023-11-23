a3d3285ab2e37d6390dc3ace0f8963dc2bd3c755
joshborup <joshborup@gmail.com>
2023-11-23 16:12:34 -0700

    blank vite template for building react libraries

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from '../'
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  )
+}
+
+export default App
